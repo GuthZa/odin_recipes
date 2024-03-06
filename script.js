9a0bfcd682f56d7cd9e70bf9f67f445fcaa50d46
Wisura <joao9.claro99@gmail.com>
2024-03-06 18:15:23 +0000

    Add elements by manipulating the DOM

---
+++ script.js
@@ -0,0 +1,23 @@
+const para = document.createElement("p");
+para.style.color = "red";
+para.textContent = "Hey I'm red";
+
+const headThree = document.createElement("h3");
+headThree.style.color = "blue";
+headThree.textContent = "I'm blue h3!";
+
+const div = document.createElement("div");
+div.style.backgroundColor = "pink";
+div.style.borderColor = "black";
+
+const h1 = document.createElement("h1");
+h1.textContent = "I'm in a div";
+
+const paraTwo = document.createElement("p");
+paraTwo.textContent = "ME TOO!";
+
+document.body.appendChild(para);
+document.body.appendChild(headThree);
+div.appendChild(h1);
+div.appendChild(paraTwo);
+document.body.appendChild(div);
